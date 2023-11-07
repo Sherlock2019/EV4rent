db5ba29b9c95802c435adcdc2d0b2edde293276e
SHERLOCK2019 <36204380+Sherlock2019@users.noreply.github.com>
2023-11-07 12:53:47 +0700

    Create variables.tf

---
+++ terraform/modules/api_gateway/variables.tf
@@ -0,0 +1,7 @@
+# modules/api_gateway/variables.tf
+
+variable "ev4rent_api_gateway_name" {
+  description = "The name of the API Gateway"
+  type        = string
+  default     = "EV4rent-API-Gateway"
+}
